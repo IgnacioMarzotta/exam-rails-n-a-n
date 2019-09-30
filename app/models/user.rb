fab5909e0ccd8560aeb15359579c4be43ba25bd2
Ignacio Marzotta <marzottanacho@gmail.com>
2019-09-30 14:07:47 -0300

    Finished adding relations between user, task & todo. Added stle to navbar, notices, login & register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :add_tasks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :todos
   has_many :tasks, through: :todos
+
+  private
+
+  def add_tasks
+    Task.all.each do |task|
+      Todo.create(
+        task_id: task.id,
+        user_id: self.id
+      )
+    end 
+  end 
+
+  def update
+    todo = Todo.find(params[:todo_id])
+    todo.completed == !todo.completed
+    todo.save
+    redirect_to root_path
+  end
 end
